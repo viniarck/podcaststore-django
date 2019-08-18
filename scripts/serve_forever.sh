1ca701b5f80db3cfce4decf2be81f4142e01afe9
Vinicius Arcanjo <viniarck@gmail.com>
2019-08-18 20:23:35 -0300

    Fixed scripts/serve_forever.sh

--- scripts/serve_forever.sh
+++ scripts/serve_forever.sh
@@ -1,19 +1,9 @@
 #!/bin/sh
 
-if [ -z "$NEW_RELIC_CONFIG_FILE" ]; then
-  if [ -z "$GUNICORN_HTTPS" ]; then
-    echo "Starting gunicorn over HTTP..."
-    gunicorn -w "$GUNICORN_WORKERS" -b 0.0.0.0:8000 cadastro_site.wsgi
-  else
-    echo "Starting gunicorn over HTTPS..."
-    gunicorn -w "$GUNICORN_WORKERS" -b 0.0.0.0:8000 --keyfile certs/privkey1.pem --certfile certs/fullchain1.pem cadastro_site.wsgi
-  fi
+if [ -z "$GUNICORN_HTTPS" ]; then
+  echo "Starting gunicorn over HTTP..."
+  gunicorn -w "$GUNICORN_WORKERS" -b 0.0.0.0:8000 podcaststore_site.wsgi
 else
-  if [ -z "$GUNICORN_HTTPS" ]; then
-    echo "Starting gunicorn with relic over HTTP..."
-    newrelic-admin run-program gunicorn -w "$GUNICORN_WORKERS" -b 0.0.0.0:8000 cadastro_site.wsgi
-  else
-    echo "Starting gunicorn with relic over HTTPS..."
-    newrelic-admin run-program gunicorn -w "$GUNICORN_WORKERS" -b 0.0.0.0:8000 --keyfile certs/privkey1.pem --certfile certs/fullchain1.pem cadastro_site.wsgi
-  fi
+  echo "Starting gunicorn over HTTPS..."
+  gunicorn -w "$GUNICORN_WORKERS" -b 0.0.0.0:8000 --keyfile certs/privkey1.pem --certfile certs/fullchain1.pem podcaststore_site.wsgi
 fi
