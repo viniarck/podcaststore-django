5dad5fca4d92260f5d0b8692a84d26657100ff1b
Vinicius Arcanjo <viniarck@gmail.com>
2019-08-25 20:34:57 -0300

    Fixed static files on settings.py

--- podcaststore_site/settings.py
+++ podcaststore_site/settings.py
@@ -40,10 +40,10 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django_extensions",
     "rest_framework",
-    "podcaststore_api",
     "rest_framework_swagger",
     "drf_yasg",
     "corsheaders",
+    "podcaststore_api",
 ]
 
 MIDDLEWARE = [
@@ -171,4 +171,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/static"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
